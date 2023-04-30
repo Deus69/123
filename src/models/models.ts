b71b29a49ca0b22c373b9e737eb0bd601c71d38a
Deus69 <xchrisovicx@mail.ru>
2023-04-30 22:38:43 +0300

    create models

---
+++ src/models/models.ts
@@ -0,0 +1,44 @@
+/** @format */
+
+import DataBase from "../database/database";
+import DataTypes from "sequelize";
+
+const User = DataBase.define("user", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, unique: true },
+  password: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING, defaultValue: "USER" },
+});
+
+const Basket = DataBase.define("basket", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const ConstructionMachinery = DataBase.define("constructionMachinery", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: false, allowNull: false },
+  description: { type: DataTypes.STRING },
+  quantity: { type: DataTypes.INTEGER, defaultValue: 1 },
+  Availability: { type: DataTypes.BOOLEAN, defaultValue: true },
+});
+
+const Type = DataBase.define("type", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+});
+
+const TrackedorWheeled = DataBase.define("trackedorwheeled", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+});
+
+User.hasOne(Basket);
+Basket.belongsTo(User);
+
+Type.hasMany(ConstructionMachinery);
+ConstructionMachinery.belongsTo(Type);
+
+TrackedorWheeled.hasMany(ConstructionMachinery);
+ConstructionMachinery.belongsTo(TrackedorWheeled);
+
+export { User, Basket, ConstructionMachinery, Type, TrackedorWheeled };
